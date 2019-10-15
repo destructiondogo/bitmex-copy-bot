363d0dbbfa7c03dcc7309d9b3cbedac5d31180a7
DestructionDogo <you@example.com>
2019-10-15 08:27:47 +0530

    Support for copy to/from multiple accounts with staking

--- config.py
+++ config.py
@@ -1,10 +1,44 @@
 TESTNET_URL = "https://testnet.bitmex.com/api/v1/"
-BITMEX_URL = "https://bitmex.com/api/v1/"
+MAINNET_URL = "https://bitmex.com/api/v1/"
 
-ENDPOINT = TESTNET_URL
+LEADERS = {
+    'LEADER_A' : {
 
-API_KEY = 'rCkoD7Pq2hF'
-API_SECRET = 'seiJbiBGcrxrPBmgsV'
+        'API_KEY' : 'GJC80Kr4BwnghuulKl6VprTH',
+        'API_SECRET' : 'POWn13jhwNO0N8Wg1GA0BnvybM_r8Jwbaa52igRGgaiZOk7B',
+        'ENDPOINT' : TESTNET_URL
 
-API_KEY_2 = 'dXNl4nImxP_fwgor'
-API_SECRET_2 = 'dfPHP8IVHs1rgPJP2eq'
+    },
+    'LEADER_B' : {
+
+        'API_KEY' : 'dsdBfhkqpQ6imYGbXhZTqyUt',
+        'API_SECRET' : 'JaT8I0zlSIrH4uXCTYBnhQNBZitVkccyt14MODImrO1MhvS2',
+        'ENDPOINT': TESTNET_URL
+
+    }
+
+}
+
+FOLLOWERS = {
+    'FOLLOWER_A' : {
+
+        'API_KEY': 'NwXfK-nQhrbQ8vDJOb8T-B4_',
+        'API_SECRET': 'fRIyyzTdns_ZQIk-LSk2XHl7ncv_aZ5fD2hUt7CNs2_kHy5d',
+        'ENDPOINT': TESTNET_URL,
+        'FOLLOWS' : {
+            'LEADER_A' : '30%',
+            'LEADER_B' : '20%'
+        }
+
+    },
+    'FOLLOWER_B' : {
+
+        'API_KEY': 'jfGfGY_tj8THivDzl7Xd3BJp',
+        'API_SECRET': 'En8htoBGqeiCgOY5d-inj6_vvgDJodwcj5WxBwpKffGjHd_E',
+        'ENDPOINT': TESTNET_URL,
+        'FOLLOWS' : {
+            'LEADER_B' : '110%'
+        }
+
+    }
+}
--- run.py
+++ run.py
@@ -3,40 +3,70 @@ from time import sleep
 
 from bitmex_api import BitMEX
 from bitmex_websocket import BitMEXWebsocket
-from config import ENDPOINT, API_KEY, API_SECRET, API_KEY_2, API_SECRET_2
+from config import LEADERS, FOLLOWERS
 
 
 def run():
     logger = setup_logger()
 
+    ws = {}
+    api = {}
+
+    leader_follower_map = {}
+    for follower in FOLLOWERS:
+        for leader in FOLLOWERS[follower]['FOLLOWS']:
+            if leader_follower_map.get(leader) is None:
+                leader_follower_map[leader] = {}
+                leader_follower_map[leader][follower] = FOLLOWERS[follower]['FOLLOWS'][leader]
+            else:
+                leader_follower_map[leader][follower] = FOLLOWERS[follower]['FOLLOWS'][leader]
+
     # Instantiating the WS will make it connect. Be sure to add your api_key/api_secret.
-    ws = BitMEXWebsocket(endpoint=ENDPOINT, symbol="",
-                         api_key=API_KEY, api_secret=API_SECRET)
+    for leader in LEADERS:
+        ws[leader] = BitMEXWebsocket(endpoint=LEADERS[leader]['ENDPOINT'], symbol="",
+                                     api_key=LEADERS[leader]['API_KEY'], api_secret=LEADERS[leader]['API_SECRET'])
+        logger.info("Instrument data: %s" % ws[leader].get_instrument())
 
-    logger.info("Instrument data: %s" % ws.get_instrument())
+    for follower in FOLLOWERS:
+        api[follower] = BitMEX(base_url=FOLLOWERS[follower]['ENDPOINT'], apiKey=FOLLOWERS[follower]['API_KEY'], apiSecret=FOLLOWERS[follower]['API_SECRET'])
 
-    api = BitMEX(base_url=ENDPOINT, apiKey=API_KEY_2, apiSecret=API_SECRET_2)
+
     executions_copied = {}
 
     # Run forever
-    while(ws.ws.sock.connected):
-        # logger.info("Market Depth: %s" % ws.market_depth())
-        executions = ws.executions()
+    while True:
+        for ws_leader in ws:
 
-        for execution in executions:
-            execID = execution['execID']
-            if execID not in executions_copied:
-                executions_copied[execID] = execution
-                if execution['execType'] == 'New':
-                    if execution['ordType'] == 'Limit':
-                        api.place_order_limit(execution['orderQty'],execution['price'],execution['symbol'],execution['side'],execution['orderID'])
-                    elif execution['ordType'] == 'Market':
-                        api.place_order_market(execution['orderQty'],execution['symbol'],execution['side'],execution['orderID'])
-                elif execution['execType'] == 'Canceled':
-                    api.cancelByCl([execution['orderID']])
+            if ws[ws_leader].ws.sock.connected:
+                # logger.info("Market Depth: %s" % ws.market_depth())
+                executions = ws[ws_leader].executions()
 
+                for execution in executions:
+                    execID = execution['execID']
+                    if execID not in executions_copied:
+                        executions_copied[execID] = execution
+
+                        for follower in leader_follower_map[ws_leader]:
+
+                            if execution['execType'] == 'New':
+                                if execution['ordType'] == 'Limit':
+                                    api[follower].place_order_limit(percentage(execution['orderQty'],leader_follower_map[ws_leader][follower]), execution['price'], execution['symbol'],
+                                                          execution['side'], execution['orderID'])
+                                elif execution['ordType'] == 'Market':
+                                    api[follower].place_order_market(percentage(execution['orderQty'],leader_follower_map[ws_leader][follower]), execution['symbol'], execution['side'],
+                                                           execution['orderID'])
+                            elif execution['execType'] == 'Canceled':
+                                api[follower].cancelByCl([execution['orderID']])
+            else:
+                #reconnect
+                logger.error("websocket disconnect, restart")
+                pass
 
         sleep(1)
+
+
+def percentage(quantity, percentage):
+    return round(quantity * float(percentage.strip('%'))/100.0)
 
 
 def setup_logger():
